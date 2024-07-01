38493059751350c00d79089e9b8fc73162660921
hartikarora2004 <hartikarora2502@gmail.com>
2024-07-01 09:45:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# CodeForces_Ratings_Racer
+This tool generates an animated race video of your and your friends' CodeForces ratings
+
+<!-- ## Sample Output
+![Raings-race-video.gif](https://media.giphy.com/media/WLMIeAeC68ZMaPsTw2/giphy.gif) -->
+
+## Dependencies
+- [requests](https://pypi.org/project/requests/)
+- [pandas](https://pypi.org/project/pandas/)
+- [BeautifulSoup](https://pypi.org/project/beautifulsoup4/)
+- [bar_chart_race](https://github.com/dexplo/bar_chart_race)
+- [ffmpeg](https://www.ffmpeg.org/download.html)
+
+## Installation
+<!-- ```sh
+pip3 install requests pandas beautifulsoup4
+``` -->
+```sh
+python -m pip install git+https://github.com/dexplo/bar_chart_race
+```
+#### Note:
+Don't install [bar_chart_race](https://github.com/dexplo/bar_chart_race) using pip as the version installed via pip seems to be outdated.
+
+### [ffmpeg](https://www.ffmpeg.org/download.html) installation:
+To install ffmpeg, you can go to [this site](https://ffmpeg.org/download.html).
+For Windows, you can directly download zip from [here](https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip), extract it and add it to path or paste it in your System32 folder.
+To understand more about the step-by-step guide on how to do this, check [this site](https://www.wikihow.com/Install-FFmpeg-on-Windows).
+
+## Usage
+Write all the CodeForces user handles in [input.txt](https://github.com/TheViking733n/CodeForces_Ratings_Racer/blob/main/input.txt) file and then run the below command:
+```sh
+python cf_racer.py
+```
---
+++ cf_racer.py
@@ -0,0 +1,230 @@
+import os
+import sys
+import ast
+import requests
+import itertools
+import threading
+import pandas as pd
+import bar_chart_race as bcr
+from datetime import datetime as dt
+from time import sleep
+
+
+# Configuration Variables
+PAUSE_IN_END = 3                  # In seconds
+OUTPUT_FILE_NAME = "output.mp4"   # use .gif extension to make gif
+READ_INPUT_FROM_FILE = True       # If False or file doesn't exists, then takes input from user
+INPUT_FILE_NAME = "input.txt"     # To store CodeForces user handles in separate lines
+VIDEO_TITLE = "Codeforces Ratings Racer"     # Default title of video
+USE_HANDLE_INSTEAD_OF_NAME = True            # If False, then it will show actual name of user in video
+
+
+# Gobal Variables
+CONTESTS = {}      # Contains a dictionary of contest_date_obj: contest_name
+USERS_RATING = {}  # Nested dictionary to store ratings of all users
+DATES = []         # List of all contest dates objects
+REAL_NAME = {}     # Distionary of handle: name
+DONE = False
+
+
+def take_handle_input():
+    """
+    Takes input from a file input.txt and returns a list of handles
+    If file doesn't exists then takes input from user
+    """
+
+    global READ_INPUT_FROM_FILE
+
+    if not os.path.exists("input.txt") or not READ_INPUT_FROM_FILE:
+        # File doesn't exists' so taking input from user
+        handles = []
+        while 1:
+            handle = input("Enter CodeForces handle (or press Enter if done): ")
+            if handle == "":
+                break
+            handles.append(handle)
+        return handles
+
+    with open(INPUT_FILE_NAME, "r") as f:
+        handles = f.readlines()
+    return [h.strip() for h in handles]
+
+
+def parse_string_date(date_time_str):
+    """
+    Takes a string of the form "Feb/23/2022" and returns a date object
+    """
+    return dt.strptime(date_time_str.split()[0], "%b/%d/%Y").date()
+
+
+def ratings_scraper(handle):
+    """
+    Takes a user handle and returns a list of their CF ratings and date of contest
+    Returns empty list if user has not given any contest
+    Returns None if user doesn't exists or HTTP error occurs
+    """
+    
+    global CONTESTS
+    global REAL_NAME
+
+    url = f"https://codeforces.com/api/user.rating?handle={handle}"
+    resp = requests.get(url)
+
+    if resp.status_code == 200:
+        data = resp.json()
+        if data['status'] != 'OK':
+            return None
+        
+        result = data['result']
+        ratings_data = {}
+        
+        for contest in result:
+            contest_name = contest['contestName']
+            contest_name = contest_name.replace("Round ", "").replace("Codeforces", "CF")
+            contest_date = dt.fromtimestamp(contest['ratingUpdateTimeSeconds']).date()
+            contest_rating = contest['newRating']
+            CONTESTS[contest_date] = contest_name
+            ratings_data[contest_date] = contest_rating
+        
+        REAL_NAME[handle] = handle  # Use handle as the name
+        return ratings_data
+    
+    else:
+        print(f"Error: Could not connect to Codeforces API for handle {handle}")
+        print(f"HTTP error: {resp.status_code}")
+        return None
+
+
+def loading():
+    sleep(1)      # Initial pause; Could be removed
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        global DONE
+        if DONE:
+            break
+        sys.stdout.write('\rIt may take few minutes... ' + c)
+        sys.stdout.flush()
+        sleep(0.1)
+    sys.stdout.write('\rDone!                              ')
+
+
+def main():
+    global USERS_RATING
+    global CONTESTS
+    global DATES
+    global REAL_NAME
+    global DONE
+    global INPUT_FILE_NAME
+    global OUTPUT_FILE_NAME
+    global VIDEO_TITLE
+    global USE_HANDLE_INSTEAD_OF_NAME
+
+    user_handles = take_handle_input()
+    # Removing duplicates
+    user_handles = list(set(user_handles))
+
+    if len(user_handles) == 0:
+        print(f"\nPlease Enter CodeForces user handles list in {INPUT_FILE_NAME} file and try again.\n")
+        return
+
+    for user in user_handles:
+        print(f"Fetching data for {user}")
+        ratings_data = ratings_scraper(user)
+
+        if ratings_data is None:
+            print(f"Unable to fetch ratings for {user}\n")
+            user_handles.remove(user)
+            continue
+
+        elif ratings_data == {}:
+            print(f"User {user} has not participated in any contests\n")
+            user_handles.remove(user)
+            continue
+
+        else:
+            USERS_RATING[user] = ratings_data
+            print(f"{len(ratings_data)} Contests participated by {user}\n")
+    
+
+    DATES = list(CONTESTS.keys())
+    DATES.sort()
+    DATES += [DATES[-1]]*PAUSE_IN_END*2    # To pause the video in the end; Because one period is 500ms
+
+    # Making a name list to for each bar title
+    if USE_HANDLE_INSTEAD_OF_NAME:
+        name_list = user_handles
+    else:
+        name_list = []
+        for handle in user_handles:
+            real_name = REAL_NAME[handle]
+            first_two = real_name.split()[:2]
+            name_list.append(" ".join(first_two))
+
+
+    # Making pandas dataframe and merging all user data of contests
+    df = pd.DataFrame(columns=name_list, index=DATES)
+
+    prev_row = [0]*len(user_handles)
+
+    for date in DATES:
+        cur_row = []
+        for i in range(len(user_handles)):
+            handle = user_handles[i]
+            if date in USERS_RATING[handle]:
+                # User gave the contest
+                cur_row.append(USERS_RATING[handle][date])
+            else:
+                # User didn't gave the contest and his ratings will remain same
+                cur_row.append(prev_row[i])
+        df.loc[date] = cur_row
+        prev_row = cur_row
+
+
+    # Convert datatypes of columns from object to int64
+    df.index = pd.to_datetime(df.index)
+    df[name_list] = df[name_list].apply(pd.to_numeric)
+
+
+    # Output file name
+    print(f"\nEnter output filename with extension (default: {OUTPUT_FILE_NAME})")
+    print("Supported formats are: .mp4, .gif, .html, .mpeg, .mov")
+    temp = input("Enter filename (or press Enter to use default): ")
+    if temp != "":
+        if not (temp.endswith(".mp4") or temp.endswith(".gif") or temp.endswith(".html") or temp.endswith(".mpeg") or temp.endswith(".mov")):
+            print("Invalid file extension. Using default")
+        OUTPUT_FILE_NAME = temp
+
+
+    # Video title
+    print(f"\nEnter video title (default: {VIDEO_TITLE})")
+    temp = input("Enter title (or press Enter to use default): ")
+    if temp!="":
+        VIDEO_TITLE = temp
+
+
+    # Now creating video of the dataframe using bar_chart_racer
+    print("\nCreating video of the contest data of all the users\n")
+    print("It may take few minutes...", end="")
+    threading.Thread(target=loading).start()
+    bcr.bar_chart_race(
+        df=df,
+        end_period_pause=50,
+        period_template='%B %d, %Y',
+        colors='dark12',
+        title=VIDEO_TITLE,
+        bar_label_font=6,
+        tick_label_font=9,
+        fixed_max=True,
+        filter_column_colors=True,
+        period_length=500,
+        filename=OUTPUT_FILE_NAME
+    )
+
+    DONE = True
+    sleep(.2)
+    print("\n\n\nThanks for using CF Racer")
+    print("See you soon")
+
+
+
+if __name__ == "__main__":
+    main()
